05f7fae8bb6d766ae432ca34a8da909a66dc96b9
Rshefa <114087782+Rshefa@users.noreply.github.com>
2023-12-14 20:53:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,30 +9,64 @@ async function getData() {
     rows.forEach((elem) => {
               const row = elem.split(",");
               const year = row[0];
-              const temp = row[1];
+              const temp = parseFloat(row[1]); 
+          
 
               years.push(year)
               temps.push(temp + 14)
 
-              console.log(year, temp);
+             
             });
+
+            return { years, temps};
 
           }
     //end of functions
     //call the function to test if you see the image on the left in the console
-    const chart = new Chart(ctx, {
-        type: 'line',
-        data: data,
-        options: {
-            scales: {
-                y: {
-                    ticks: {
-                        // Include a dollar sign in the ticks
-                        callback: function(value, index, ticks) {
-                            return '$' + value;
+
+
+    async function getChart() {
+        const ctx = document.getElementById('myChart');
+        const data = await getData();
+        const adjustedTemps = data.temps.map(temp => (temp)); // Convert temps to numbers
+    
+        const chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: data.years,
+                datasets: [{
+                    label: 'Global Avg Temp',
+                    data: adjustedTemps,
+                    backgroundColor: 'rgba(75, 192, 192, 0.2)', // Background color of the line
+                    borderColor: 'rgba(75, 192, 192, 1)', // Border color of the line
+                    borderWidth: 2, // Width of the line
+                    pointRadius: 4, // Radius of data points
+                    pointBackgroundColor: 'rgba(75, 192, 192, 1)', // Background color of data points
+                    pointBorderColor: '#fff', // Border color of data points
+                    pointHoverRadius: 6, // Hover radius of data points
+                    pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)', // Hover background color of data points
+                    pointHoverBorderColor: 'rgba(220, 220, 220, 1)', // Hover border color of data points
+                    pointHitRadius: 10, // Hit radius of data points
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        
+                        min: 13, // Set your desired minimum temperature
+                        max: 15.2, // Set your desired maximum temperature
+                         
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            fontSize: 16, // Set the font size for the label
                         }
                     }
                 }
             }
-        }
-    });
+        });
+    }
+  
+    
